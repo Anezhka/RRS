a790ecd87d0d5e59377c225121cf7c96939b0ba3
Anezhka <40922520+Anezhka@users.noreply.github.com>
2025-02-28 18:24:14 +0100

    Lesson9_HW

--- src/Homework/Lesson9/Month.java
+++ src/Homework/Lesson9/Month.java
@@ -6,7 +6,7 @@ package Homework.Lesson9;
  * Создать класс MonthUtils который бы хранил подготовленные месяцы или их массивы для использования (объекты класса Month).
  */
 
-public class Month {
+public final class Month {
 
     private final String monthName;
     private final int days;
@@ -17,15 +17,12 @@ public class Month {
         this.days = days;
         this.workingDays = workingDays;
     }
-
     public String getMonthName() {
         return monthName;
     }
-
     public int getDays() {
         return days;
     }
-
     public int getWorkingDays() {
         return workingDays;
     }
